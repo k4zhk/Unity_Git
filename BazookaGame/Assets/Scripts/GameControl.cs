1812a819cf4cb2f0244e6884d19519114fda6c7c
k4zhk <ROMARQUE@Kazuhiko-no-MacBook-Pro.local>
2014-11-24 00:20:28 +0900

    day2
    初心者でもできる！ 7日間で作るUnityゲーム開発

---
+++ BazookaGame/Assets/Scripts/GameControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+	public GameObject bear;
+	
+	void Start () {
+		for (int i = 0; i < 9; i++) {
+			Instantiate(bear);
+		}
+	}
+	
+	void Update () {
+	}
+}
